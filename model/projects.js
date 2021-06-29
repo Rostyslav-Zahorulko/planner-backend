b67d510d179cfa7449deb44fcadc569ed584220c
Serhii Koziuba <preborist@gmail.com>
2021-06-29 18:10:49 +0300

    added routes for sprints

--- model/projects.js
+++ model/projects.js
@@ -5,12 +5,12 @@ const getAllPojects = async () => {
   return results;
 };
 
-const getProjectById = async (projectId) => {
+const getProjectById = async projectId => {
   const result = await Project.findOne({ _id: projectId });
   return result;
 };
 
-const createProject = async (body) => {
+const createProject = async body => {
   const result = await Project.create(body);
   return result;
 };
@@ -21,13 +21,78 @@ const updateProject = async (projectId, body) => {
       _id: projectId,
     },
     { ...body },
-    { new: true }
+    { new: true },
   );
   return result;
 };
 
-const removeProject = async (projectId) => {
+const removeProject = async projectId => {
   const result = await Project.findByIdAndRemove({ _id: projectId });
+  return result;
+};
+
+// CREATING SPRINTS
+const createSprint = async (projectId, body) => {
+  const currentSprints = await getProjectById(projectId);
+  // console.log(currentSprints.sprints);
+  const result = await Project.findOneAndUpdate(
+    {
+      _id: projectId,
+    },
+    { sprints: [...currentSprints.sprints, body] },
+    { new: true },
+  );
+  return result;
+};
+
+const getAllSprints = async () => {
+  const results = await Project.find({});
+  return results;
+};
+
+const getSprintById = async (projectId, sprintId) => {
+  const project = await Project.find({ _id: projectId });
+  const [{ sprints }] = project;
+  const getsprint = sprints.find(sprint => sprint._id == sprintId);
+  return getsprint;
+};
+
+const removeSprintById = async (projectId, sprintId) => {
+  const project = await Project.find({ _id: projectId });
+  const [{ sprints }] = project;
+  const findsprint = await sprints.find(sprint => sprint._id == sprintId);
+  if (findsprint) {
+    const getsprint = sprints.filter(sprint => sprint._id != sprintId);
+    const result = await Project.findOneAndUpdate(
+      {
+        _id: projectId,
+      },
+      { sprints: [...getsprint] },
+      { new: true },
+    );
+    return result;
+  }
+};
+
+const updateSprintById = async (projectId, sprintId, body) => {
+  const project = await Project.find({ _id: projectId });
+  const [{ sprints }] = project;
+  const updatedSprints = await sprints.map(sprint => {
+    if (sprint._id == sprintId) {
+      const { _id, duration, startDate } = sprint;
+      const updatedSprint = { _id, ...body, duration, startDate };
+      console.log(updatedSprint);
+      return updatedSprint;
+    } else return sprint;
+  });
+
+  const result = await Project.findOneAndUpdate(
+    {
+      _id: projectId,
+    },
+    { sprints: [...updatedSprints] },
+    { new: true },
+  );
   return result;
 };
 
@@ -37,4 +102,9 @@ module.exports = {
   createProject,
   updateProject,
   removeProject,
+  createSprint,
+  getAllSprints,
+  getSprintById,
+  removeSprintById,
+  updateSprintById,
 };
--- routes/api/projects.js
+++ routes/api/projects.js
@@ -6,6 +6,11 @@ const {
   createProject,
   updateProject,
   removeProject,
+  createSprint,
+  getAllSprints,
+  getSprintById,
+  removeSprintById,
+  updateSprintById,
 } = require('../../model/projects');
 const {
   validateCreateProject,
@@ -76,6 +81,114 @@ router.patch('/:projectId', validateUpdateProject, async (req, res, next) => {
 router.delete('/:projectId', async (req, res, next) => {
   try {
     const projectWithId = await removeProject(req.params.projectId);
+    if (projectWithId) {
+      return res
+        .status(200)
+        .json({ status: 'success', code: 200, data: { projectWithId } });
+    }
+    return res
+      .status(404)
+      .json({ status: 'error', code: 404, message: 'Not found' });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// CREATE SPRINT
+// ФОРМАТ ДЖЕЙСОНА ДЛЯ ЗАПРОСА
+// {
+//     "title": "test sprint 5",
+//     "duration": 10,
+//     "startDate": "December 17, 1995 03:24:00"
+// }
+router.post('/:projectId/sprints', async (req, res, next) => {
+  try {
+    const updatedProject = await createSprint(req.params.projectId, req.body);
+    if (updatedProject) {
+      return res
+        .status(200)
+        .json({ status: 'success', code: 200, data: { updatedProject } });
+    }
+    return res
+      .status(404)
+      .json({ status: 'error', code: 404, message: 'Not found' });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET ALL SPRINTS AND PROJECT INFORMATION
+
+router.get('/:projectId/sprints/', async (_req, res, next) => {
+  try {
+    const project = await getAllSprints();
+    const [{ sprints, id, title, description, team }] = project;
+
+    return res.status(200).json({
+      status: 'success',
+      code: 200,
+      data: {
+        sprints,
+        projectInfo: { id, title, description, team },
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET SPRINT BY ID
+
+router.get('/:projectId/sprints/:sprintId', async (req, res, next) => {
+  try {
+    const sprint = await getSprintById(
+      req.params.projectId,
+      req.params.sprintId,
+    );
+
+    if (sprint) {
+      return res
+        .status(200)
+        .json({ status: 'success', code: 200, data: { sprint } });
+    }
+    return res
+      .status(404)
+      .json({ status: 'error', code: 404, message: 'Not found' });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// REMOVE SPRINT BY ID
+
+router.delete('/:projectId/sprints/:sprintId', async (req, res, next) => {
+  try {
+    const result = await removeSprintById(
+      req.params.projectId,
+      req.params.sprintId,
+    );
+    if (result) {
+      return res
+        .status(200)
+        .json({ status: 'success', code: 200, data: { result } });
+    }
+    return res
+      .status(404)
+      .json({ status: 'error', code: 404, message: 'Not found' });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// UPDATE SPRINT TITLE BY ID
+
+router.patch('/:projectId/sprints/:sprintId', async (req, res, next) => {
+  try {
+    const projectWithId = await updateSprintById(
+      req.params.projectId,
+      req.params.sprintId,
+      req.body,
+    );
     if (projectWithId) {
       return res
         .status(200)
